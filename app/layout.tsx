3df7fe045fa7bfeef629eee2656bc3869ed1aa26
Cineticmotion <cineticmotion_th@Cineticmotionui-MacBookPro.local>
2026-08-07 11:24:26 +0900

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import { cookies } from "next/headers";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import { LanguageProvider } from "@/lib/i18n";
